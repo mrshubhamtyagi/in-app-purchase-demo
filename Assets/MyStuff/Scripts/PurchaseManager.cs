6ecbbbf383e0cdd808b80c2e77ccd0129574ffa6
Shubham Tyagi <styagi.2301@gmail.com>
2019-05-22 00:43:26 +0530

    Few changes

---
+++ Assets/MyStuff/Scripts/PurchaseManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PurchaseManager : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    public void Buy99Coins()
+    {
+        IAPManager.INSTANCE.BuyCoins99();
+    }
+
+    public void Buy499Coins()
+    {
+        IAPManager.INSTANCE.BuyCoins499();
+    }
+
+}
